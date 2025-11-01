74a078a764d5088ce1886fe1aad747adaa34b69e
Matt Mina <mina.matthew12@gmail.com>
2025-11-01 09:41:53 +0800

    Refactor Expense into Transaction

---
+++ Tribby.Core/Classes/Transaction.cs
@@ -0,0 +1,19 @@
+﻿namespace Tribby.Core.Classes
+{
+    public class Transaction
+    {
+        public string Description { get; set; }
+
+        public float Amount { get; set; }
+
+        public string Payer { get; set; }
+
+        public Transaction(string description, float amount, string payer)
+        {
+            Description = description;
+            Amount = amount;
+            Payer = payer;
+        }
+
+    }
+}
