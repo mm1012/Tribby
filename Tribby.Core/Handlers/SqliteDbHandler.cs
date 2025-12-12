af7b7073c5e668648c30c2c8a236da5b19b45ff8
Matthew Mina <matthewm@corebridge.net>
2025-12-12 17:23:39 +0800

    Add method to get groups

--- Tribby.Core/Handlers/SqliteDbHandler.cs
+++ Tribby.Core/Handlers/SqliteDbHandler.cs
@@ -1,4 +1,7 @@
+using System.Collections.Immutable;
 using System.Data.Common;
+using System.Reflection.Metadata;
+using System.Runtime.CompilerServices;
 using Microsoft.EntityFrameworkCore;
 
 namespace Tribby.Core.Handlers
@@ -12,21 +15,27 @@ namespace Tribby.Core.Handlers
 
         private static string _dbPath = $"{dbName}";
 
-        public TribbyDbContext? DbContext { get; private set; }
+        public static TribbyDbContext? DbContext { get; private set; }
 
         public SqliteDbHandler()
         {
-            var folder = Environment.SpecialFolder.LocalApplicationData;
-            var path = Environment.GetFolderPath(folder);
+            var path = @"..\..\..\..\";
             _dbPath = Path.Join(path, "Tribby.db");
         }
 
-
-        public void Connect()
+        public async void Connect()
         {
             if (DbContext == null)
             {
                 DbContext = new TribbyDbContext();
+
+                //DbContext.Database.EnsureDeleted();
+                DbContext.Database.EnsureCreated();
+
+                // DbContext.Add(new User { Id = 1, Name = "Matt", GroupId = 1});
+                // DbContext.Add(new User { Id = 2, Name = "Levine", GroupId = 1});
+                // DbContext.Add(new Group { Balance = 0, Id = 1, Name = "BBB"});
+                // await DbContext.SaveChangesAsync();
             }
         }
 
@@ -49,6 +58,26 @@ namespace Tribby.Core.Handlers
 
             DbContext.Dispose();
             DbContext = null;
+        }
+
+        public async Task<List<Group>> GetGroups()
+        {
+            if (DbContext == null)
+            {
+                return new List<Group>();
+            }
+            
+            var groups = await DbContext.Groups
+                .FromSqlRaw(@"select * from Groups where Id == 1")
+                .SingleAsync();
+                //             left join Groups g on g.Id = gu.GroupsId")
+            
+            // var users = await DbContext.Entry(groups)
+            //     .Collection(b => b.Users)
+            //     .LoadAsync();
+
+
+            return new List<Group>{groups};
         }
     }
 }
